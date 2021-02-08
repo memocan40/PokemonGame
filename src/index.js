fae74e566003edb4709e5880ed42a3553f744f81
memo40 <memocann4040@gmail.com>
2021-02-08 17:11:06 +0100

    setted up react routers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import{BrowserRouter} from "react-router-dom"
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+<BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
