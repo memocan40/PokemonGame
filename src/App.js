e604301c18dc3f2d3427931e3183c287c62de54a
memo40 <memocann4040@gmail.com>
2021-02-10 22:08:16 +0100

    changed styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import {Switch,Route,Link}from "react-router-dom";
 import Pokemon from "./views/AllPokemon";
-import PokemonyID from "./views/PokemonByID";
+import PokemonID from "./views/PokemonByID";
 import PokemonByIDByInfo from "./views/PokemonByIDByInfo";
 import Search from "./component/Search";
 
@@ -26,11 +26,16 @@ function App() {
     <div className="App">
     
     <Search/>
-   <Pokemon name={inputvalue}/>
+   
       <Switch>
     <Route path="/AllPokemon">
-    <h1>here shoud be the backend data</h1>
-    
+    <Pokemon />
+    </Route>
+    <Route path="/PokemonByID">
+      <PokemonID/>
+    </Route>
+    <Route path="/PokemonByIDByInfo">
+      <PokemonByIDByInfo/>
     </Route>
         
       </Switch>
