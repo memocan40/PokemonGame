77dd5613cb2126d50bdaf1d74351b42ebdecc54d
memo40 <memocann4040@gmail.com>
2021-02-10 14:00:05 +0100

    test view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import {Switch,Route}from "react-router-dom";
+import Pokemon from "./views/View";
 
 function App() {
   return (
     <div className="App">
+    <h1>test</h1>
+    <Pokemon poke="testit"/>
       <Switch>
 
         
---
+++ src/views/View.js
@@ -0,0 +1,10 @@
+export default function Pokemon({poke}){
+    return(
+        <div>
+        <h1>Pokemon</h1>
+        <br></br>
+        <p>name {poke}</p>
+
+        </div>
+    )
+}
