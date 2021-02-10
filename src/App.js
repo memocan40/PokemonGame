f5f9ecc541fc72f0924eb97073e86552e9108fff
memo40 <memocann4040@gmail.com>
2021-02-10 20:25:12 +0100

    setted up basics on frontend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import { useState } from "react";
+import "./App.css";
 import {Switch,Route,Link}from "react-router-dom";
 import Pokemon from "./views/AllPokemon";
 import PokemonyID from "./views/PokemonByID";
 import PokemonByIDByInfo from "./views/PokemonByIDByInfo";
 
 function App() {
-  let[link,setlink]=useState("test");
-  
+  let[dropdownvalue,setdropdownvalue]=useState("");
+  let[inputvalue,setinputvalue]=useState();
+  let[result,setresult]=useState();
+  const Clickhandler=()=>{
+    if(dropdownvalue===""){
+      setresult("/"+inputvalue);
+    }
+    else{
+      setresult("/"+inputvalue+"/"+dropdownvalue);
+      
+    }
+  }
   return (
     <div className="App">
-    <input type="text" placeholder="Type a ID"/>
-    <section class="Button">
     
-    <Link to="/AllPokemon"><button>Get All</button></Link>
-    </section>
-    <section class="Dropdown">
+    
+    <div className="Button">
+    
+    <Link to="/AllPokemon"><button className="Items">Get All</button></Link>
+    </div>
+    <div className="Dropdown">
+
+    <div className="Input"><input className="Items" type="text" placeholder="Type a ID" onChange={(event)=>{setinputvalue(event.target.value)}} /></div>
       <label for="Dropdown">Choose a Info:</label>
-  <select name="Dropdown" id="dropdown" onChange={(event)=>{setlink(event.target.value)}}>
+  <select name="Dropdown" id="dropdown" className="Items" onChange={(event)=>{setdropdownvalue(event.target.value)}}>
+    <option value="" ></option>
     <option value="Name" >Name</option>
     <option value="Base">Base</option>
     <option value="Type">Type</option>
-    
-    
   </select>
   <br></br>
-  <input type="submit" value="Submit"  ></input>
-    </section>
-    <div>{link}</div>
+  <Link to={result}><input className="Items" onClick={Clickhandler} type="submit" value="Submit"  ></input></Link>
+    </div>
+    
     <Pokemon name="test" base="base" type="code"/>
       <Switch>
     <Route path="/AllPokemon">
